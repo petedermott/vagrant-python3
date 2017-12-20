ddb3dc2b1aa1e30e2df2ece321c3f044015031ed
Peter Dermott <p.dermott@fermorltd.co.uk>
2017-12-20 17:33:00 +0000

    update readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## vagrant-python3
+
+Ships with Python 3.6, 2.7, and virtualenvwrapper
+
+Based on the excellent work of FlipperPA at https://github.com/FlipperPA/django-python3-vagrant, I just bought everything up to date.
+
+Yes this should probably be a fork but I'm still learning how to git properly.
---
+++ vagrant_bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install git for version control, pip for install python packages
+echo 'Installing git, Python 3, and pip...'
+# libfreetype6-dev ziblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get install python3.6
+sudo apt-get -qq install git python3 libjpeg-dev libtiff5-dev zlib1g-dev > /dev/null 2>&1
+curl -s https://bootstrap.pypa.io/get-pip.py | python3.6 > /dev/null 2>&1
+
+# Install virtualenv / virtualenvwrapper
+echo 'Installing and configuring virtualenv and virtualenvwrapper...'
+pip install --quiet virtualenvwrapper Pygments
+mkdir ~vagrant/virtualenvs
+chown vagrant:vagrant ~vagrant/virtualenvs
+printf "\n\n# Virtualenv settings\n" >> ~vagrant/.bashrc
+printf "export PYTHONPATH=/usr/lib/python3.6" >> ~vagrant/.bashrc
+printf "export WORKON_HOME=~vagrant/virtualenvs\n" >> ~vagrant/.bashrc
+printf "export PROJECT_HOME=/vagrant\n" >> ~vagrant/.bashrc
+printf "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6\n" >> ~vagrant/.bashrc
+printf "source /usr/local/bin/virtualenvwrapper.sh\n" >> ~vagrant/.bashrc
+
+# Some useful aliases for getting started, MotD
+echo 'Setting up message of the day, and some aliases...'
+sudo cp /vagrant/examples/motd.txt /etc/motd
+printf "\nUseful Aliases:\n" >> ~vagrant/.bashrc
+printf "alias menu='cat /etc/motd'\n" >> ~vagrant/.bashrc
+printf "alias runserver='python manage.py runserver 0.0.0.0:8000'\n" >> ~vagrant/.bashrc
+printf "alias ccat='pygmentize -O style=monokai -f terminal -g'\n" >> ~vagrant/.bashrc
+
+# Complete
+echo ""
+echo "Vagrant install complete."
+echo "Now try logging in:"
+echo "    $ vagrant ssh"
+
